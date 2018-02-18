7f13cd6dd5893509a2d281ca899139d2bf98a87c
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-02-17 22:04:18 -0600

    0.2.0 a lot changed

--- legume.js
+++ legume.js
@@ -1,243 +1,132 @@
-legumeload = function() {
-  delete legumeload;
-  function _throw(e) {
-    throw e;
-  }
+window.legumeload = function() {
+  delete window.legumeload;
   var entry = document.querySelector("script[src*=legume]");
-  var AsyncFunction;
-  try {
-    AsyncFunction = Object.getPrototypeOf(eval("async () => {}")).constructor;
-  } catch (err) {}
-  function processurl(inurl) {
-    var relative;
+  function parseURL(inurl) {
     try {
       inurl = new URL(inurl);
     } catch (err) {
       if (err.message == "Failed to construct 'URL': Invalid URL") {
-        relative = inurl;
         inurl = new URL(inurl, location.href);
       }
     }
+    var ret = {};
     var methods = ["github", "npm"];
     for (var i = 0; i < methods.length; i++) {
       var cur = methods[i];
       if (inurl.protocol == cur + ":") {
         return {
-          url: {
-            fullURL:
-              "https://cdn.jsdelivr.net/" +
+          url: new URL(
+            "https://cdn.jsdelivr.net/" +
               (cur == "github" ? "gh" : cur) +
               "/" +
               inurl.pathname
-          },
-          method: cur
+          ),
+          name: inurl.pathname
+            .split("/")
+            [cur == "github" ? 1 : 0].split("@")[0],
+          version:
+            inurl.pathname.split("/")[cur == "github" ? 1 : 0].split("@")[1] ||
+            "latest",
+          method: cur,
+          legumescript: cur == "npm" ? true : undefined
         };
       }
     }
-    return { url: { fullURL: inurl.href, relURL: relative } };
+    return { url: inurl };
   }
-  function load(inurl, msg, method, pmd) {
-    pmd = Object.assign(processurl(inurl), pmd || {});
-    var purl = pmd.url;
-    return fetch(purl.fullURL)
-      .then(function(r) {
-        if (r.ok) {
-          return r[method]();
-        } else {
-          throw new Error(msg);
-        }
-      })
-      .then(function(r) {
-        return { res: r, md: pmd };
-      })
-      .catch(alert);
-  }
-  var legume = {
-    version: "dev",
-    scripts: {},
-    load: function load(input, opts) {
-      if (typeof input == "string") {
-        input = input.trim();
-      }
-      function legumestring(str) {
-        var types = ["json", "text", "script"];
-        for (var i = 0; i < types.length; i++) {
-          var cur = types[i];
-          if (str.startsWith(cur + ":")) {
-            return legume[cur](str.replace(new RegExp(cur + ":"), "").trim());
-          }
-        }
-        return legume.script(str);
-      }
-      if (typeof opts == "string") {
-        return {
-          script: legume.script,
-          json: legume.json,
-          text: legume.text,
-          txtscript: function(input) {
-            return legume.process({ res: input });
-          }
-        }[opts](input);
-      }
-      if (typeof input == "string") {
-        return legumestring(input);
-      } else if (Array.isArray(input)) {
-        var retarr = [];
-        input.forEach(function(cur) {
-          retarr.push(legumestring(cur));
-        });
-        return Promise.all(retarr);
-      }
-    },
-    process: function process(loadres, pmd) {
-      return Promise.resolve().then(function() {
-        var parsed = parse(loadres.res, Object.assign({}, loadres.md, pmd)),
-          meta = parsed.metadata,
-          code = parsed.code,
-          waitScript = new Promise(function(resolve) {
-            if (!meta.script) {
-              resolve();
-            } else {
-              var scripts = meta.script;
-              var exports = {};
-              loop(0);
-              function loop(i) {
-                var cur = scripts[i];
-                return legume.script(cur.script).then(function(exp) {
-                  exports[cur.as] = exp;
-                  if (scripts.length != i + 1) {
-                    loop(i + 1);
-                  } else {
-                    resolve(exports);
-                  }
+  var legume = Object.assign(
+    function legume(input, dontload) {
+      var output = {};
+      Object.assign(output, parseURL(input));
+      parse(output);
+      output.name = output.name || output.metadata.name;
+      return legume.scripts[output.name]
+        ? require(output.name)
+        : fetch(output.url)
+            .then(function(r) {
+              return r.text();
+            })
+            .then(function(code) {
+              output.code = code;
+              legume.scripts[output.name] = output;
+              var ret = Promise.resolve(),
+                requires = output.requires;
+              if (requires.styles)
+                requires.styles.forEach(function(cur) {
+                  cur = parseURL(cur).url;
+                  document.head.appendChild(
+                    Object.assign(document.createElement("link"), {
+                      rel: "stylesheet",
+                      href: cur.href
+                    })
+                  );
                 });
-              }
-            }
-          });
-        if (meta.style) {
-          meta.style.forEach(legume.style);
-        }
-        ["var", "async", "url", "method"].forEach(function(cur) {
-          parsed[cur] = parsed.metadata[cur];
-          delete parsed.metadata[cur];
-        });
-        var url = parsed.url;
-        if (url) {
-          code += url.fullURL ? "\n//# sourceURL=" + url.fullURL : "";
-        }
-        parsed.var = parsed.var || [];
-        parsed.var = parsed.var.reduce(function(obj, cur) {
-          var split = cur.split(" ");
-          obj[split[0]] = split[1] || split[0];
-          return obj;
-        }, {});
-        var module = {
-            exports: {}
-          },
-          exports = module.exports,
-          vars = {
-            module: module,
-            exports: exports
-          };
-        return waitScript.then(function(deps) {
-          var depsnames = (meta.script || []).reduce(function(obj, cur, i) {
-            if (cur.name) obj[cur.name] = Object.values(deps)[i];
-            return obj;
-          }, {});
-          Object.assign(vars, deps, {
-            require: function require(mod) {
-              return depsnames[mod];
-            }
-          });
-          Object.assign(
-            parsed.var,
-            Object.keys(deps || {}).reduce(function(obj, cur) {
-              obj[cur] = cur;
-              return obj;
-            }, {})
-          );
-          var ret;
-          if (
-            (parsed.legumescript && parsed.metadata.name) ||
-            parsed.method == "npm"
-          ) {
-            var namespace = (legume.scripts[meta.name] =
-              legume.scripts[meta.name] || parsed);
-            namespace.clicks = namespace.clicks + 1 || 0;
-            ret = Promise.resolve().then(function() {
-              return new (Function.prototype.bind.apply(
-                parsed.async
-                  ? AsyncFunction
-                    ? AsyncFunction
-                    : _throw(
-                        new Error(
-                          "Async Functions not supported in this browser"
-                        )
-                      )
-                  : Function,
-                [null]
-                  .concat(Object.values(parsed.var))
-                  .concat(parsed.method == "npm" ? ["module", "exports"] : [])
-                  .concat([code])
-              ))().apply(
-                namespace,
-                Object.keys(parsed.var)
-                  .concat(parsed.method == "npm" ? ["module", "exports"] : [])
-                  .map(function(cur) {
-                    return vars[cur];
-                  })
-              );
+              if (requires.scripts)
+                ret = ret.then(function() {
+                  return Promise.all(
+                    requires.scripts.reduce(function(arr, scr) {
+                      arr.push(legume(scr.script, true));
+                      return arr;
+                    }, [])
+                  );
+                });
+              return ret.then(function() {
+                if (!dontload) require(output.name);
+              });
             });
-          } else {
-            eval.call(null, code);
-            ret = Promise.resolve();
-          }
-          return ret.then(function() {
-            return !Object.getOwnPropertyNames(module.exports).length &&
-              !Object.getOwnPropertySymbols(module.exports).length
-              ? undefined
-              : module.exports;
-          });
-        });
-      });
     },
-    script: function script(scrurl) {
-      return load(scrurl, "Couldn't load the script.", "text").then(
-        legume.process
-      );
-    },
-    style: function style(stlurl) {
-      var l = document.createElement("link");
-      l.href = processurl(stlurl);
-      document.head.append(l);
-      l.rel = "stylesheet";
-    },
-    json: function json(jsonurl) {
-      return load(jsonurl, "Couldn't load JSON.", "json").then(function(r) {
-        return r.res;
-      });
-    },
-    text: function text(txturl) {
-      return load(txturl, "Couldn't load text.").then(function(r) {
-        return r.res;
-      });
+    {
+      version: "dev",
+      scripts: {},
+      cache: {},
+      require: require,
+      global: {}
     }
-  };
+  );
   window.Legume = legume;
-  if (entry) legume.load(entry);
-  function parse(data, pmd) {
-    // pmd == provided metadata
+  if (entry && entry.getAttribute("data-legume-entry"))
+    Legume(entry.getAttribute("data-legume-entry"));
+  function require(mod) {
+    mod = legume.scripts[mod];
+    if (mod) {
+      if (legume.cache[mod.name]) return legume.cache[mod.name];
+      var possargs = {
+        module: { exports: {} },
+        require: require,
+        global: legume.global
+      };
+      possargs.exports = possargs.module.exports;
+      var passargs = ["module", "exports", "require", "global"];
+      var requires = mod.requires;
+      if (requires.scripts) {
+        requires.scripts.forEach(function(cur) {
+          if (cur.as && !Object.keys(possargs).includes(cur.as)) {
+            possargs[cur.as] = require(parseURL(cur.script).name);
+            passargs.push(cur.as);
+          }
+        });
+      }
+      var code = mod.code + (mod.url ? "\n//# sourceURL=" + mod.url : "");
+      if (mod.legumescript) {
+        Function.apply(null, passargs.concat([code])).apply(
+          legume.global,
+          passargs.reduce(function(arr, cur) {
+            arr.push(possargs[cur]);
+            return arr;
+          }, [])
+        );
+        legume.cache[mod.name] = possargs.module.exports;
+        return possargs.module.exports;
+      } else {
+        return eval(code);
+      }
+    }
+  }
+  function parse(script) {
     var inBlock = false,
       cmt = {
-        cmt: /^(\s*\/\/\s*)/,
-        multi: {
-          start: /^(\s*\/\*\s*@legume\s*)/i,
-          end: /^\s*\*\//
-        },
-        single: {
-          start: /\s*@legume\s*/,
-          match: /\s*@[^@]*/g
-        }
+        start: /^(\s*\/\*\s*@legume\s*)/i,
+        end: /^\s*\*\//
       },
       md = {
         name: "",
@@ -249,14 +138,11 @@ legumeload = function() {
         url: "",
         license: "",
         script: [],
-        style: [],
-        var: [],
-        async: false
+        style: []
       },
       options = {},
       code = [],
       errors = [],
-      legumescript = false,
       processCmt = function(comment) {
         var match = comment.trim().match(/@([^\s]+)(?:\s+(.*))?$/);
         if (match) {
@@ -280,26 +166,14 @@ legumeload = function() {
           }
         }
       };
-    data.match(/[^\r\n]+/g).forEach(function(line, i, lines) {
-      if (cmt.cmt.test(line)) {
-        var comment = line.replace(cmt.cmt, "").trim();
-        if (cmt.single.start.test(comment)) {
-          comment.replace(cmt.single.start, "");
-          for (
-            var m = cmt.single.match.exec(comment);
-            m;
-            m = cmt.single.match.exec(comment)
-          ) {
-            processCmt(m[0]);
-          }
-        }
-      } else if (cmt.multi.end.test(line) && inBlock) {
+    script.code.match(/[^\r\n]+/g).forEach(function(line, i, lines) {
+      if (cmt.end.test(line) && inBlock) {
         inBlock = false;
       } else if (inBlock) {
         processCmt(line);
-      } else if (cmt.multi.start.test(line)) {
+      } else if (cmt.start.test(line)) {
         inBlock = true;
-        legumescript = true;
+        script.legumescript = true;
       } else {
         code.push(line);
       }
@@ -307,27 +181,28 @@ legumeload = function() {
         errors.push("missing metdata block closing");
       }
     });
-    var metadata = Object.assign(options, pmd);
-    if (metadata.script)
-      metadata.script.forEach(function(cur, i) {
+    var requires = {
+      scripts: options.script,
+      styles: options.style
+    };
+    delete options.script;
+    delete options.style;
+    if (requires.scripts)
+      requires.scripts.forEach(function(cur, i) {
         var arr = cur.split(" ");
         var obj = {};
         for (var j = 1; j < arr.length; j += 2) {
           var temparr = arr.slice(j, j + 2);
           obj[temparr[0]] = temparr[1];
         }
-        metadata.script[i] = { script: arr[0], as: obj.as, name: obj.name };
+        requires.scripts[i] = { script: arr[0], as: obj.as || null };
       });
-    return {
-      metadata: metadata,
+    return Object.assign(script, {
+      metadata: options,
       code: code.join("\n"),
       errors: errors.length ? errors : null,
-      legumescript: legumescript
-    };
-  }
-  var entryScript = entry.dataset.legumeEntry;
-  if (entryScript) {
-    Legume.script(entryScript);
+      requires
+    });
   }
 };
 (function() {
