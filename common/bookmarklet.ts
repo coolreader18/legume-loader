00dd8c652196e7c3c423af3152820ba247aa597f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-09-26 23:19:10 -0500

    Remove cli, move bookmarklet url functionality to another file

---
+++ common/bookmarklet.ts
@@ -0,0 +1,11 @@
+export const makeUrl = (legumeVer: string, script: string) =>
+  `javascript:(function(t,l,d,s,n){\
+typeof Legume==n+""?(\
+s=d.createElement("script"),\
+s.src=l,s.setAttribute("data-legume-entry",t),\
+d.head.appendChild(s)\
+):\
+Legume(t,{run:true})\
+})(${JSON.stringify(script)},${JSON.stringify(
+    `https://cdn.jsdelivr.net/npm/legume-loader@${legumeVer || "latest"}`
+  )},document);`;
